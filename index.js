b88b6d2105105c26fa5c0cd94bdd995b7e94c2c8
isabellepedrosoferreira-hub <isabelle.pedroso.ferreira@escola.pr.gov.br>
2026-05-14 20:52:18 +0000

    Trabalho 1 Finalizado

---
+++ index.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+
+console.log(chalk.magentaBright('A estrutura de um conto é organizada de forma simples, mas muito eficiente para contar uma história curta com impacto. Por ser um gênero narrativo breve, o conto precisa apresentar os elementos essenciais de uma história de maneira rápida e bem concentrada.'));
+
