23dcdc7939373e24ab37d5cc62f63d2aad00d162
isabellepedrosoferreira-hub <isabelle.pedroso.ferreira@escola.pr.gov.br>
2026-05-19 17:31:22 +0000

    Trabalho 2 Finalizado

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.magentaBright('A estrutura de um conto é organizada de forma simples, mas muito eficiente para contar uma história curta com impacto. Por ser um gênero narrativo breve, o conto precisa apresentar os elementos essenciais de uma história de maneira rápida e bem concentrada.'));
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
 
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro)
+// }
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
+
+//console.log(chalk.magentaBright('A estrutura de um conto é organizada de forma simples, mas muito eficiente para contar uma história curta com impacto. Por ser um gênero narrativo breve, o conto precisa apresentar os elementos essenciais de uma história de maneira rápida e bem concentrada'));
